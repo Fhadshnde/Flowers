400688fde75fefc620428580db24c0a9346ded4b
fahad <fhadshnde32@gmail.com>
2024-12-26 05:08:26 +0300

    first commit

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/posts" + search);
+        setPosts(res.data);
+      } catch (err) {
+        console.error("Failed to fetch posts:", err);
+      }
+    };
+    fetchPosts();
+  }, [search]);
+
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
